486533e3f562056c25cf80a8516a80d4f73d206e
Marcelo Diniz <marcelo.leo27@gmail.com>
2017-09-26 16:23:52 -0300

    more tests

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,15 @@
 <div id="main">
 	<h1><?= $a ?></h1>
 
-	{{ message }}
+	<p>Mensagem original: {{ message }}</p>
+
+	<p>Mensagem ao contrário: {{ reversedMessage }}</p>
+
+	<p>Mensagem ao contrário por método: {{ mReversedMessage() }}</p>
+
+	<p>Data: {{ now }}</p>
+	<p>Data por método: {{ mNow() }}</p>
+
+	<div v-html="rawHtml"></div>
+
 </div>
--- assets/js/first_test_vue.js
+++ assets/js/first_test_vue.js
@@ -1,8 +1,25 @@
-console.log('teste');
-
-var app = new Vue({
+var vm = new Vue({
   el: '#main',
   data: {
-    message: 'Olá Vue!'
+    message: 'Olá Vue!',
+    rawHtml: '<h3>Teste com html</h3>'
+  },
+  computed : {
+    // uma função "getter" computada (computed getter)
+    reversedMessage: function() {
+      // `this` aponta para a instância Vue da variável `vm`
+      return this.message.split('').reverse().join('')
+    },
+    now: function() {
+      return Date.now()
+    }
+  },
+  methods : {
+    mReversedMessage: function() {
+      return this.message.split('').reverse().join('')
+    },
+    mNow: function() {
+      return Date.now()
+    }
   }
 })
