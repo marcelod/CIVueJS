325bdc79dc9cf2a93b766881507b69cc9ce2d556
Marcelo Diniz <marcelo.leo27@gmail.com>
2017-09-27 17:46:34 -0300

    filters

--- application/views/bulma.php
+++ application/views/bulma.php
@@ -5,4 +5,12 @@
 	<button :class="btnClassB" @click.prevent.stop="enviar()">Botão B</button>
 	<button :class="btnClassC">Botão C</button>
 
+	<hr>
+
+	<ul>
+		<li v-for="user in users">
+			{{ user.name | toUpperCase | truncate(7) }}
+		</li>
+	</ul>
+
 </div>
--- assets/js/test_bulma_vue.js
+++ assets/js/test_bulma_vue.js
@@ -22,44 +22,35 @@ var vm = new Vue({
       'is-outlined' : true,
       'is-inverted' : true,
       'is-outlined' : false,
-    }
+    },
+    users: [
+      { name: 'Marcelo Diniz' },
+      { name: 'Vanessa Cicolani' },
+      { name: 'Thomas Cicolani Diniz' }
+    ]
+
   },
+
   methods : {
     enviar() {
       alert('Enviado!')
     },
-    // mNow: function() {
-    //   return Date.now()
-    // }
-
-  }
-    /*message: 'Olá Vue!',
-    rawHtml: '<h3>Teste com html</h3>',
-    user: {
-      id: 1,
-      name: 'Marcelo'
-    },
-    linguagens: [
-      { name: 'JavaScript' },
-      { name: 'PHP' },
-      { name: 'CSS' },
-      { name: 'Python' },
-      { name: 'C++' },
-    ],
-    image: "https://cdn.pixabay.com/photo/2016/03/05/19/10/airplane-1238277_960_720.jpg",
-    altImage: "test vue"
-
-  computed : {
-    // uma função "getter" computada (computed getter)
-    reversedMessage: function() {
-      // `this` aponta para a instância Vue da variável `vm`
-      return this.message.split('').reverse().join('')
-    },
-    now: function() {
-      return Date.now()
-    }
   },
-  */
+
+  filters: {
+    toUpperCase(str) {
+      return str.toUpperCase();
+    },
+
+    truncate(str, length) {
+      if (str.length > length) {
+        return str.substring(0, length) + ' ...';
+      }
+
+      return str;
+    }
+  }
+
 })
 
 
