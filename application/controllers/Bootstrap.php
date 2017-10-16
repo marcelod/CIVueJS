adbb46feeda2fd5fd1745cd305b54f21c09cf843
Marcelo Diniz <marcelo.leo27@gmail.com>
2017-10-16 14:31:06 -0200

    remove duplicate lines

--- application/controllers/Bootstrap.php
+++ application/controllers/Bootstrap.php
@@ -3,26 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Bootstrap extends MY_Controller {
 
-	public function index()
+	public function __construct()
 	{
+		parent::__construct();
+
 		$this->data['css'] = load_css('bootstrap.min');
 		$this->data['js'] = load_js([
-			'test_bootstrap_vue',
 			'jquery.min',
 			'bootstrap.min'
 		]);
+	}
+
+	public function index()
+	{
+		$this->data['js'].= load_js(['test_bootstrap_vue']);
 
 		$this->render();
 	}
 
 	public function ajax_resource()
 	{
-		$this->data['css'] = load_css('bootstrap.min');
-		$this->data['js'] = load_js([
+		$this->data['js'].= load_js([
 			'vue-resource.min',
 			'test_bootstrap_ajax_resource_vue',
-			'jquery.min',
-			'bootstrap.min'
 		]);
 
 		$this->render('bootstrap_ajax_resource');
@@ -30,12 +33,9 @@ class Bootstrap extends MY_Controller {
 
 	public function ajax_axios()
 	{
-		$this->data['css'] = load_css('bootstrap.min');
 		$this->data['js'] = load_js([
 			'axios.min',
 			'test_bootstrap_ajax_axios_vue',
-			'jquery.min',
-			'bootstrap.min'
 		]);
 
 		$this->render('bootstrap_ajax_axios');
